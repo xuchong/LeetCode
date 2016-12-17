a037ca8dcd81cc04fd81fc8b6f8c735a5aa3ba6f
xc <xuchongone@hotmail.com>
2016-12-17 21:47:42 +0800

    add PalindromicSubstring

---
+++ LongestPalindromicSubstring/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+class Solution {
+public:
+	string longestPalindrome(string s) {
+		
+	}
+};
+int main()
+{
+	string a;
+	while (true)
+	{
+		cin >> a;
+		
+	}
+}
