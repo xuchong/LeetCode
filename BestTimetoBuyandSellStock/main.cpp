0e8408821a223cedd2db1cd9c1143244fd586029
xuchong_mac <xuchongone@hotmail.com>
2017-07-19 01:42:29 +0800

    BestTimetoBuyandSellStock. easy

---
+++ BestTimetoBuyandSellStock/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+      int _min;
+      if(prices.size()==0)
+        return 0;
+      _min=prices[0];
+      int result=0;
+      for(int i=1;i<prices.size();i++)
+      {
+        _min=min(_min,prices[i]);
+        if(result<prices[i]-_min)
+        {
+          result=prices[i]-_min;
+        }
+      }
+    }
+};
+int main()
+{
+  return 0;
+}
