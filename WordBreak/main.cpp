ea7ead21d6bd818ea6891bcbad9fd434ac3dcb53
xuchong_mac <xuchongone@hotmail.com>
2017-08-06 16:29:09 +0800

    good job! well done.  :relaxed:

--- WordBreak/main.cpp
+++ WordBreak/main.cpp
@@ -34,7 +34,27 @@ class Solution{
 public:
   bool wordBreak(string s,vector<string>& wordDict)
   {
-    
+    unordered_set<string> dict(wordDict.begin(),wordDict.end());
+    vector<vector<bool>> dp(s.length(),vector<bool>(s.length(),false));
+    if(dict.find(s.substr(0,1))!=dict.end())
+      dp[0][0]=true;
+    for(int i=1;i<s.length();i++)
+    {
+      if(dict.find(s.substr(0,i+1))!=dict.end())
+      {
+        dp[0][i]=true;
+        continue;
+      }
+      //get dp[0][i]
+      for(int j=0;j<i;j++)
+      {
+        if(dp[0][j]&&dict.find(s.substr(j+1,i-j))!=dict.end())
+        {
+            dp[0][i]=true;
+        }
+      }
+    }
+    return dp[0][s.length()-1];
   }
 };
 #endif
