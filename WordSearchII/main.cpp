7adab8ac9c75f0058ea7a371322b5f4041e93404
xuchong_mac <xuchongone@hotmail.com>
2017-09-01 19:28:32 +0800

    TL. search stringII. hard enough

--- WordSearchII/main.cpp
+++ WordSearchII/main.cpp
@@ -1,42 +1,31 @@
 class Solution {
 private:
-  bool isInclude(vector<vector<char>>&board,vector<vector<char>>&copy,vector<int> word,int m,int n)
+  bool isInclude(vector<vector<char>>&board,vector<vector<char>>&copy,string word,int index,int m,int n)
   {
-      if(copy[m][n]=='#'||word[board[m][n]-'a']==0)
+      if(index>=word.length()||copy[m][n]=='#'||board[m][n]!=word[index])
       {
         return false;
-      }else
-      {
-        word[board[m][n]-'a']--;
       }
-      bool result=true;
-      for(int i=0;i<word.size();i++)
-      {
-        if(word[i]!=0)
-        {
-          result=false;
-          break;
-        }
-      }
-      if(result)
-        return result;
+      if(index==word.length()-1)
+        return true;
+      bool result=false;
       //four
       copy[m][n]='#';
       if(m-1>=0)
       {
-        result+=isInclude(board,copy,word,m-1,n);
+        result+=isInclude(board,copy,word,index+1,m-1,n);
       }
       if(m+1<board.size())
       {
-        result+=isInclude(board,copy,word,m+1,n);
+        result+=isInclude(board,copy,word,index+1,m+1,n);
       }
       if(n-1>=0)
       {
-        result+=isInclude(board,copy,word,m,n-1);
+        result+=isInclude(board,copy,word,index+1,m,n-1);
       }
       if(n+1<board[0].size())
       {
-        result+=isInclude(board,copy,word,m,n+1);
+        result+=isInclude(board,copy,word,index+1,m,n+1);
       }
       copy[m][n]=board[m][n];
       return result;
@@ -46,16 +35,12 @@ public:
       vector<string> result;
       if(board.empty()||words.empty())
         return result;
-      vector<unordered_map<string,vector<int>>> startsWith(26,unordered_map<string,vector<int>>());
+      vector<vector<string>> startsWith(26,vector<string>());
       string word;
       for(int i=0;i<words.size();i++)
       {
         word=words[i];
-        startsWith[word[0]-'a'][word]=vector<int>(26,0);
-        for(int j=0;j<word.length();j++)
-        {
-          startsWith[word[0]-'a'][word][word[j]-'a']++;
-        }
+        startsWith[word[0]-'a'].push_back(word);
       }
       unordered_set<string> result_set;
       vector<vector<char>> copy_board(board);
@@ -67,11 +52,11 @@ public:
           if(startsWith[board[i][j]-'a'].size()>0)
           {
             for(auto it=startsWith[board[i][j]-'a'].begin();it!=startsWith[board[i][j]-'a'].end();it++){
-              if(result_set.find(it->first)==result_set.end())
+              if(result_set.find(*it)==result_set.end())
               {
-                if(isInclude(board,copy_board,it->second,i,j))
+                if(isInclude(board,copy_board,*it,0,i,j))
                 {
-                  result_set.insert(it->first);
+                  result_set.insert(*it);
                 }
               }
             }
