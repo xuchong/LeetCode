46e8f8ebf6a7b720c359be7958e51ac67d6e3f95
xuchong_mac <xuchongone@hotmail.com>
2017-09-01 20:41:42 +0800

    why trie is faster? prefix tree or dict tree can be used to find prefix substring, it can accelerate the speed of search. :open_mouth:

--- WordSearchII/main.cpp
+++ WordSearchII/main.cpp
@@ -1,3 +1,5 @@
+#define TL
+#ifndef TL
 class Solution {
 private:
   bool isInclude(vector<vector<char>>&board,vector<vector<char>>&copy,string word,int index,int m,int n)
@@ -67,3 +69,88 @@ public:
       return result;
     }
 };
+#else
+// This is method about tie. why is faster?
+struct Trie
+{
+  string word;
+  Trie* next[26];
+  Trie():word(""){
+    memset(next,NULL,sizeof(next));
+  }
+};
+class Solution {
+private:
+  unordered_set<string> result_set;
+  void recursive(vector<vector<char>>&board,Trie* root,int m,int n)
+  {
+      Trie* next=root->next[board[m][n]-'a'];
+      if(board[m][n]=='#'||next==NULL)
+      {
+        return ;
+      }
+      if(next->word!=""){
+        result_set.insert(next->word);
+      }
+      //four
+      char tmp=board[m][n];
+      board[m][n]='#';
+      if(m-1>=0)
+      {
+        recursive(board,next,m-1,n);
+      }
+      if(m+1<board.size())
+      {
+        recursive(board,next,m+1,n);
+      }
+      if(n-1>=0)
+      {
+        recursive(board,next,m,n-1);
+      }
+      if(n+1<board[0].size())
+      {
+        recursive(board,next,m,n+1);
+      }
+      board[m][n]=tmp;
+  }
+void buildTrie(Trie* root,vector<string>& words)
+{
+  string item;
+  int index;
+  Trie* p;
+  for(int i=0;i<words.size();i++)
+  {
+    item=words[i];
+    index=0;
+    p=root;
+    while(index<item.length())
+    {
+      if(p->next[item[index]-'a']==NULL)
+      {
+        p->next[item[index]-'a']=new Trie();
+      }
+      p=p->next[item[index]-'a'];
+      index++;
+    }
+    p->word=item;
+  }
+}
+public:
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+      vector<string> result;
+      if(board.empty()||words.empty())
+        return result;
+      Trie* root=new Trie();
+      buildTrie(root,words);
+      for(int i=0;i<board.size();i++)
+      {
+        for(int j=0;j<board[0].size();j++)
+        {
+          recursive(board,root,i,j);
+        }
+      }
+      result.insert(result.end(),result_set.begin(),result_set.end());
+      return result;
+    }
+};
+#endif
