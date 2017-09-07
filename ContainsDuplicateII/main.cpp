ecde2ad8e22d48b73d57c8ec93cd083fe954cb08
xuchong_mac <xuchongone@hotmail.com>
2017-09-07 16:13:34 +0800

    set: insert,erase(key),erase(it)

---
+++ ContainsDuplicateII/main.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if(nums.empty())
+          return false;
+        int i=0,j=0;
+        bool result=false;
+        unordered_set<int> sets;
+        if(nums.size()<=k)
+        {
+          k=nums.size()-1;
+        }
+        for(j=0;j<=k;j++)
+        {
+          if(sets.find(nums[j])!=sets.end())
+            return true;
+          sets.insert(nums[j]);
+        }
+        for(j;j<nums.size();j++)
+        {
+          sets.erase(nums[j-k-1]);
+          if(sets.find(nums[j])!=sets.end())
+            return true;
+          set.insert(nums[j]);
+        }
+        return false;
+    }
+};
